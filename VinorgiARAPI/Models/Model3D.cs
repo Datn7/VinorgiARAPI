61b37714d77304c80471c0463de9793f82b76b5c
Datn7 <datunashvili72@gmail.com>
2025-07-17 13:28:16 +0400

    updated table name since in db its called Models not Models3D

--- VinorgiARAPI/Models/Model3D.cs
+++ VinorgiARAPI/Models/Model3D.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VinorgiARAPI.Models
 {
+    [Table("Models")]
     public class Model3D
     {
         public int Id { get; set; }
